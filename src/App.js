b27e622c92965530c65b8449f5f1221f80c2f2b2
Nevada686 <natalina686@gmail.com>
2022-12-11 21:36:57 +0100

    creat weather-react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+        <Weather city="Kyiv" />
       </header>
+      <div className="footer">
+        <a
+          className="link"
+          href="https://github.com/Natalina686/weather-react"
+          alt="GitHub Link"
+          target="blank"
+        >
+          Open-source code
+        </a>
+        , by Nataliia Tkach
+      </div>
     </div>
   );
 }
